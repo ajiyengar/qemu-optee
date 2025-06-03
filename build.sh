72160d469fb4ad9f7c07a77286357b404a39a32f
Ajay Iyengar <12970408+ajiyengar@users.noreply.github.com>
2025-06-02 22:20:49 -0700

    build: avoid APFS issues building optee work around two issues on macos: 1. APFS is case in-sensitive so syncing linux sources results in issues    particularly with netfilter directory which has case sensitive header    files. 2. Bind mounts do not work well -- both implementations DockerVMM and    Apple Virtualization Framework with VirtioFS still encounter issues    building glibc.
    instead, build it all inside docker and copy the final built content out

--- build.sh
+++ build.sh
@@ -10,25 +10,12 @@ DOCKERCONTAINER=jforissier/optee_os_ci:qemu_check
   && DOCKERCONTAINER=jforissier/optee_os_ci:qemu_check_arm64
 
 ###############
-# Patch
-###############
-# set +e
-# # patch --binary -d edk2 -p1 -N -i \
-#   ../0001-edk2-enable-debug-O0.patch -r-
-# [[ $? -gt 1 ]] \
-#   && {
-#     echo "ERR: EDK2 patch failed"
-#     exit 0
-#   }
-# set -e
-
-###############
 # Docker Build
 ###############
 docker run -it --rm \
   -v ~/dockerhome:/home \
   -v /tmp/docker_ccache:/root/.cache/ccache \
   -v .:/work \
-  -w /work \
+  -w /root \
   $DOCKERCONTAINER \
-  ./scripts/build_internal.sh
+  /work/scripts/build_internal.sh
--- scripts/build_internal.sh
+++ scripts/build_internal.sh
@@ -9,23 +9,35 @@ git config --global user.name "CI user"
 git config --global user.email "ci@invalid"
 git config --global color.ui false
 export LC_ALL=C
-export LANG=C.UTF_8
-ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 apt install -y ncat
 export FORCE_UNSAFE_CONFIGURE=1 # Prevent Buildroot error when building as root
 export BR2_CCACHE_DIR=/root/.cache/ccache
-rm build.log
+rm /work/build.log 2>/dev/null || true
 
-################
-## Sync
-################
-#repo --color=never init -u https://github.com/OP-TEE/manifest.git -m qemu_v8.xml -b 4.6.0
-#repo --color=never sync -j$(nproc)
-#make -C build -j$(nproc) toolchains 2>&1 | tee -a build.log
-#rm -f build/toolchains/gcc*.tar.xz
-/root/get_optee.sh qemu_v8 /work
+###############
+# Sync
+###############
+# repo --color=never init -u https://github.com/OP-TEE/manifest.git -m qemu_v8.xml -b 4.6.0
+# repo --color=never sync -j$(nproc)
+# make -C build -j$(nproc) toolchains 2>&1 | tee -a build.log
+/root/get_optee.sh qemu_v8
+
+###############
+# Patch **TEMPLATE**
+###############
+# set +e
+# # patch --binary -d edk2 -p1 -N -i \
+#   ../0001-edk2-enable-debug-O0.patch -r-
+# [[ $? -gt 1 ]] \
+#   && {
+#     echo "ERR: EDK2 patch failed"
+#     exit 0
+#   }
+# set -e
 
 ###############
 # Build
 ###############
-make -C build -j $(nproc) 2>&1 | tee -a build.log
+make -C optee/build -j$(nproc) 2>&1 | tee -a /work/build.log
+# cp -R optee/ /work/optee/
+/bin/bash
